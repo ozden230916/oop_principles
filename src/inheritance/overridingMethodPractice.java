2db859e75e12291e5e79d2b458ab5db2722ead2b
ozden230916 <oozerfirat@gmail.com>
2022-04-16 16:41:04 -0400

    method overriding

---
+++ src/inheritance/overridingMethodPractice.java
@@ -0,0 +1,13 @@
+package inheritance;
+
+public class overridingMethodPractice {
+    public static void main(String[] args) {
+        Tester tester = new Tester();
+        tester.eat();
+        tester.sleep();
+        tester.walk();
+        tester.learn();
+        tester.code();
+
+    }
+}
